0abd58cf53ac3a2a410903c8f94077cb65f49e3a
DevWonny <cjfdnjs1994@naver.com>
2022-12-04 22:55:09 +0900

    faet : server 구현 완료

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ require("dotenv").config();
 const express = require("express");
 // mongoose
 const mongoose = require("mongoose");
+const { userRouter } = require("./routes/userRouter");
+const { authentication } = require("./middleware/authentication");
 
 const { PORT, MONGO_URI } = process.env;
 
 const app = express();
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connect!");
+
+    app.use(express.json());
+    //authentication
+    app.use(authentication);
+    app.use("/user", userRouter);
+
+    app.listen(PORT, () => {
+      console.log("Express Server Listening on Port" + PORT);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const User = require("../modules/user");
+
+const authentication = async (req, res, next) => {
+  const { sessionid } = req.headers;
+
+  if (!sessionid || !mongoose.isValidObjectId(sessionid)) {
+    return next();
+  }
+
+  const user = await User.findOne({ "sessions._id": sessionid });
+
+  if (!user) {
+    return next();
+  }
+
+  req.user = user;
+  return next();
+};
+
+module.exports = { authentication };
